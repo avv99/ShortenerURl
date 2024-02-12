b30482a845adc9250198f3417a3823cfa49512e0
avv99 <72482107+avv99@users.noreply.github.com>
2024-02-12 23:15:13 +0700

    11111

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("fadsfa"))
-	})
-
-	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("fadsfa"))
-	})
 
 	log.Println("Я завелся")
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", http.DefaultServeMux)
 
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,4 @@
 module shortenerurl
 
-go 1.21.4
+go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.10
-
-require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/segmentio/analytics-go/v3 v3.3.0 // indirect
-	github.com/segmentio/backo-go v1.0.0 // indirect
-)
